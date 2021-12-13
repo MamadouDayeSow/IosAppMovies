878f4db12d64fb114e38e226441eda418f3c40ab
Mamadou Daye Sow <mamadou.sow@student.hogent.be>
2021-12-13 14:38:51 +0100

    Movieviewmodel aangemaakt

---
+++ IosAppMovies/Viewmodel/MovieViewModel.swift
@@ -0,0 +1,77 @@
+//
+//  MovieViewModel.swift
+//  IosAppMovies
+//
+//  Created by Daye Sow on 13/12/2021.
+//
+//
+//  MovieViewmodel.swift
+//  Movies
+//
+//  Created by Daye Sow on 08/12/2021.
+//
+
+import Foundation
+
+class MovieViewmodel : ObservableObject{
+    
+    
+    @Published var movies : [Movie] = []
+
+    
+    
+    init() {
+        fillWithMovies()
+    }
+    func fillWithMovies() {
+       let newMovies = [
+        Movie(name: "Inception", director: "Mamadou Sow",isWatched: true,rateMovie: 1),
+        Movie(name: "The house of hill", director: "Timon De Maegdt",isWatched: false,rateMovie: 2),
+        Movie(name: "Champions League", director: "Mo Khouder",isWatched: false,rateMovie: 3),
+        Movie(name: "My name is Sarah", director: "Flore Vl",isWatched: false,rateMovie: 4),
+        Movie(name: "Stranger things", director: "Jelle Veireman",isWatched: false,rateMovie: 1)
+        ]
+        movies.append(contentsOf : newMovies)
+    }
+    
+    func addMovie(name : String, director : String, isWatched : Bool, rateMovie : Int) {
+        movies.append(Movie(name: name, director: director,isWatched: isWatched,rateMovie: rateMovie))
+        
+    }
+    func updateMovie(movie : Movie) {
+        if var m  = movies.first(where: {$0.id == movie.id}) {
+            
+            m.name = movie.name
+            m.director = movie.director
+            m.isWatched = movie.isWatched
+            m.rateMovie = movie.rateMovie
+        }
+        
+       
+    }
+    
+    
+    func deleteMovie(indexSet : IndexSet){
+        movies.remove(atOffsets: indexSet)
+    }
+    func moveMovie(indices: IndexSet, newOffset : Int) {
+        movies.move(fromOffsets: indices, toOffset: newOffset)
+    }
+    
+    func shuffleMovies(){
+        movies.shuffle()
+    }
+    
+    func removeFirstMovie(){
+        movies.removeFirst()
+    }
+    
+    func removeLasttMovie(){
+        movies.removeLast()
+    }
+    
+    func reverseMovies(){
+        movies.reverse()
+    }
+}
+
