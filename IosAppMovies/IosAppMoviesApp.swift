1254b2476bd7402a85289988a140ac074b1dd35a
Mamadou Daye Sow <mamadou.sow@student.hogent.be>
2021-12-13 14:28:04 +0100

    Initial Commit

---
+++ IosAppMovies/IosAppMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosAppMoviesApp.swift
+//  IosAppMovies
+//
+//  Created by Daye Sow on 13/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct IosAppMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
